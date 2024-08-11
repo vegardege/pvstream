72b4a5384965a18e777910d40f385b199a59e559
Vegard Egeland <vegardegeland@gmail.com>
2024-08-11 14:35:56 +0200

    Add mutex to python iterator
    Stick closer to the example from the PyO3 documentation. Should be
    safer even if this is intended for single threaded use.

--- src/python.rs
+++ src/python.rs
@@ -4,6 +4,7 @@ use pyo3::exceptions::PyIOError;
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use std::path::Path;
+use std::sync::Mutex;
 use url::Url;
 
 impl From<RowError> for PyErr {
@@ -81,7 +82,7 @@ impl PyPageviewsRow {
 
 #[pyclass(name = "RowIterator")]
 struct PyRowIterator {
-    iter: RowIterator,
+    iter: Mutex<RowIterator>,
 }
 
 #[pymethods]
@@ -96,15 +97,17 @@ impl PyRowIterator {
             stream_from_file(path)?
         };
 
-        Ok(Self { iter })
+        Ok(Self {
+            iter: Mutex::new(iter),
+        })
     }
 
-    fn __iter__(slf: PyRefMut<Self>) -> PyRefMut<Self> {
+    fn __iter__(slf: PyRef<'_, Self>) -> PyRef<'_, Self> {
         slf
     }
 
-    fn __next__(&mut self) -> PyResult<Option<PyPageviewsRow>> {
-        match self.iter.next() {
+    fn __next__(slf: PyRefMut<'_, Self>) -> PyResult<Option<PyPageviewsRow>> {
+        match slf.iter.lock().unwrap().next() {
             Some(Ok(row)) => Ok(Some(row.into())),
             Some(Err(err)) => Err(err.into()),
             None => Ok(None),
